d95e93f042e813d1d34166745001a38abd19dc99
NgocTrungBe <bengoctrung23@gmail.com>
2021-11-22 17:31:07 +0700

    header slider

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const navItems = document.querySelectorAll('.header__nav ul li a');
 //nav click handle 
 
 navItems.forEach((item, index) => {
-
     item.addEventListener("click", function() {
         const navItemIsActive = document.querySelector('.header__nav ul li a.active');
         navItemIsActive.classList.remove("active");
