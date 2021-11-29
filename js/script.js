1fabe8e188d77814d08d47913ab22281d679cf5f
NgocTrungBe <bengoctrung23@gmail.com>
2021-11-29 16:24:09 +0700

    about section, sass standard sass directory structure

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@ navItems.forEach((item, index) => {
     item.addEventListener("click", function() {
         const navItemIsActive = document.querySelector('.header__nav ul li a.active');
         navItemIsActive.classList.remove("active");
-        this.classList.add("active")
+        this.classList.add("active");
     });
 
-})
+});
+//mobile menu handle
+const menu_icon = document.querySelector('#menu-icon');
+const nav = document.querySelector('.header__nav');
+
+
+menu_icon.addEventListener('click', function() {
+    menu_icon.classList.toggle('fa-times');
+    nav.classList.toggle('menu-toggle');
+});
+
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 100);
+});
