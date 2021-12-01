30ffd18cb7cdbcc07db12855af640e2a5ee478f9
NgocTrungBe <bengoctrung23@gmail.com>
2021-12-01 10:38:10 +0700

    history section

--- js/script.js
+++ js/script.js
@@ -22,3 +22,10 @@ const header = document.querySelector(".header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+// animation on scroll
+AOS.init({
+    offset: 120,
+    delay: 0,
+    duration: 1000,
+})
